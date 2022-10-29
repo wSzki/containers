6b168864e48093f77725bf09fecab90953d047a0
wsz <wsz@arch.home>
2022-10-29 22:04:13 +0200

    stack

--- tests/stack.cpp
+++ tests/stack.cpp
@@ -6,7 +6,7 @@
 /*   By: wszurkow <wszurkow@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/29 18:51:38 by wszurkow          #+#    #+#             */
-/*   Updated: 2022/10/29 21:36:46 by wszurkow         ###   ########.fr       */
+/*   Updated: 2022/10/29 22:04:01 by wszurkow         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@
 
 #define STACK NS::stack<T>
 
-template <typename U>
+	template <typename U>
 void compare(U & a, U & b)
 {
 	std::cout << "(a == b)       : " << Yel << (a == b) << RCol  << std::endl;
@@ -38,22 +38,63 @@ void title(std::string s)
 	std::cout << RCol;
 }
 
-void print_stack(STACK &v)
+void print_stack(STACK &st)
 {
-	std::cout << "empty   " << " : " << Yel <<  v.empty() << RCol << std::endl;
-	std::cout << "size    " << " : " << Yel <<  v.size()  << RCol << std::endl;
-	std::cout << "top     " << " : " << Yel <<  v.top()   << RCol << std::endl;
-	std:: cout << "---------------------------------" << std::endl;
+	std::cout << "empty        " << " : " << Yel <<  st.empty() << RCol << std::endl;
+	std::cout << "size         " << " : " << Yel <<  st.size()  << RCol << std::endl;
+	if (st.size() > 0)
+		std::cout << "Top of stack  : " << Yel << st.top() << RCol << std::endl;
+	else
+		std::cout << "Top of stack  : " << Yel << "[EMPTY]" << RCol << std::endl;;
+	std::cout << "---------------------------------" << std::endl;
 }
 
 int main(void)
 {
 	title("Init empty stack");
 	STACK st;
-	for (int i = 0; i < 10; i++)
+	print_stack(st);
+
+	title("Pushing 10 elements to stack");
+	for (int i = 101; i <= 110; i++)
 	{
 		st.push(i);
-		st.top();
+		print_stack(st);
 	}
+
+	STACK st_copy(st);
+	STACK st_copy_copy = st_copy;
+
+	title("Popping 2 elements from stack copy");
+	print_stack(st_copy);
+	st_copy.pop();
+	print_stack(st_copy);
+	st_copy.pop();
+	print_stack(st_copy);
+
+	title("Stack comparaison");
+	compare(st, st_copy_copy);
+	compare(st, st_copy);
+
+	title("Popping everything");
+
+	title("First stack");
+	while (st.size() > 0)
+	{
+		print_stack(st);
+		st.pop();
+	}
+	print_stack(st);
+
+	title("Copied stack");
+	while (st_copy.size() > 0)
+	{
+		print_stack(st_copy);
+		st_copy.pop();
+	}
+	print_stack(st_copy);
+
+	title("Empty stack compare");
+	compare(st, st_copy);
 
 }
